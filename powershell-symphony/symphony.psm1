9f460e3b6b92f57d6ea69f55ec67fe98bc707ef1
David Markle <dmarkle@potsol.com>
2017-10-03 15:52:00 -0400

    first commit

---
+++ powershell-symphony/symphony.psm1
@@ -0,0 +1,110 @@
+using namespace System.Security.Cryptography.X509Certificates;
+
+$curl       = 'c:\cygwin64\bin\curl.exe'
+$baseUrlApi = 'https://foundation-dev-api.symphony.com'
+$baseUrl    = 'https://foundation-dev.symphony.com'
+
+function New-Session {
+	param(
+		$password = $global:password,
+		$certPath = $global:certPath,
+		$keyPath  = $global:keyPath
+	)
+		
+	Write-Debug "Fetching session token."
+	$sessionToken = ConvertFrom-Json (& $curl -X POST  --cert "$($certPath):$password"  --key $keyPath "$baseUrlApi/sessionauth/v1/authenticate"  -H "cache-control: no-cache" 2> $null)
+	Write-Debug "Session token is $sessionToken"
+	Write-Debug "Fetching key token."
+	$keyManagerToken  = ConvertFrom-Json (& $curl -X POST --cert "$($certPath):$password"  --key $keyPath "$baseUrlApi/keyauth/v1/authenticate"  -H "cache-control: no-cache" 2> $null)
+	Write-Debug "Key token is $keyToken"
+	
+	$session = @{
+		sessionToken=$sessionToken.token
+		keyManagerToken = $keyManagerToken.token
+		certPath = $certPath
+		keyPath = $keyPath
+		password = $password
+	}
+	
+	$global:LastSession = $session
+	$session
+}
+
+function Get-Streams {
+	param (
+		$session=$global:LastSession
+	)
+	
+	$array = ConvertFrom-Json (& $curl -X POST "$baseUrl/pod/v1/streams/list" --cert "$($session.certPath):$($session.password)"  --key $session.keyPath -H "sessionToken: $($session.sessionToken)" 2>$null )
+	foreach ($item in $array) {
+		$item
+	}
+}
+
+function Get-Message {
+	param (
+		$session=$global:LastSession,		
+		[parameter(ValueFromPipeline=$true)]	
+		$stream,
+		[Parameter(Mandatory=$true)]
+		[datetime]$Since
+	)	
+	
+	begin {		
+		$timeT = ($Since | ConvertTo-TimeT )
+	}
+	process {
+		$safeStreamId = Get-SafeStreamId $stream
+		Write-Debug "$baseUrl/agent/v4/stream/$safeStreamId/message?since=$timeT"
+		$array = ConvertFrom-Json (& $curl -X GET "$baseUrl/agent/v4/stream/$safeStreamId/message?since=$timeT" --cert "$($session.certPath):$($session.password)"  --key $session.keyPath -H "sessionToken: $($session.sessionToken)" -H "keyManagerToken: $($session.keyManagerToken)" 2>$null )
+		
+		foreach ($item in $array) {
+			$item
+		}
+	}
+}
+
+function ConvertTo-TimeT {
+	param(
+		[parameter(ValueFromPipeline)]	
+		$date
+	)
+	($date - [DateTime]::new(1970, 1, 1)).TotalSeconds
+}
+
+function Get-SafeStreamId {
+	param(
+		$stream
+	)
+	
+	if ( $stream -is [string]) {
+		$streamId = $stream		
+	} else {
+		$streamId = $stream.id
+	}
+		
+	$streamId -replace '/', '_' -replace '==$', '' -replace '=', '' -replace '\+', '-'	
+}
+
+function Send-Message {
+	param (
+		[Parameter(Mandatory=$true)]
+		$stream,
+		$session=$global:LastSession,
+		[parameter(ValueFromPipeline)]
+		$message = ""
+	)
+	
+	Process {	
+		$safeStreamId = Get-SafeStreamId $stream
+		ConvertFrom-Json (& $curl -X POST "$baseUrl/agent/v4/stream/$safeStreamId/message/create" --cert "$($session.certPath):$($session.password)"  --key $session.keyPath -H "Content-Type: multipart/form-data" -H "sessionToken: $($session.sessionToken)" -H "keyManagerToken: $($session.keyManagerToken)"  --form-string "message=<messageML>$message</messageML>" 2>$null)
+	}
+	
+}
+
+Export-ModuleMember Send-Message
+Export-ModuleMember New-Session
+Export-ModuleMember Get-Streams
+Export-ModuleMember Get-Message
+
+
